e3dfb34a59c1d9d8bd03396f09e626132a80b5b3
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-09-20 23:39:16 +0200

    Add: Component Content to view mobile with styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Header } from "./components/Header";
+import { Content } from "./components/Content/index";
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Content/>
     </div>
   );
 }
---
+++ src/components/Content/index.js
@@ -0,0 +1,21 @@
+import './Content.scss';
+
+
+function  Content () {
+  return (
+    <main>
+      <div className='home__information'>
+        <div className='text'>SO, YOU WANT TO TRAVEL TO</div>
+        <h1 className='home__title'>Space</h1>
+        <p className='home__text'>
+          Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!
+        </p>
+      </div>
+      <div className='home__image'>
+      <div className='home__image-text'>EXPLORE</div>
+      </div>
+    </main>
+  )
+}
+
+export { Content }
