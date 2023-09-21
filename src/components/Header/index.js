c8bc11002fd1ce43376e99713d73ce691949c6d1
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-09-21 19:23:11 +0200

    Feat: Add header to view tablet with styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,6 +6,22 @@ function Header () {
       <div className="header__left"></div>
       <div className="header__right">
         <div className='hamburger__icon'></div>
+        <nav className='header__right--desktop'>
+          <ul>
+            <li>
+              <a>HOME</a>
+            </li>
+            <li>
+              <a>DESTINATION</a>
+            </li>
+            <li>
+              <a>CREW</a>
+            </li>
+            <li>
+              <a>TECHNOLOGY</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   )
