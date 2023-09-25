c9fdb9385b29adcc1321430b948129a3f1860440
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-09-25 21:18:14 +0200

    Feat: Implement react-router-dom for routing

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import {Header} from '../components/Header/index';
+import {Content} from '../components/Content/index';
+
+function Home() {
+  return (
+    <>
+      <Header/>
+      <Content/>
+    </>
+  )
+}
+
+export { Home };
