18aaf50ddad9038f81b52149a7183f8da6b0fe4a
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-10-01 21:34:47 +0200

    Feat: Implemented dynamic display of destinations

--- src/containers/Destination/index.js
+++ src/containers/Destination/index.js
@@ -1,28 +1,34 @@
-import moon from '../../assets/destination/background-destination-tablet.jpg';
+import { useState } from 'react';
+import data from '../../assets/data/data.json';
 import './Destination.scss';
 
 function Destination() {
+  const [destination, setDestination] = useState("MOON");
+  const selectedDestination =data.destination.find(item=>item.name === destination);
+  const destinationImage = require(`../../assets/destination/image-${selectedDestination.name.toLowerCase()}.png`);
+
+  const handleDestination=(name) => {
+    setDestination(name);
+  }
   return (
     <>
       <div className="destination__container">
       <div className="destination__title"><span>01</span> PICK YOUR DESTINATION</div>
-      <img className="destination__image" src={moon}></img>
+      <img className="destination__image" src={destinationImage}></img>
       <div className="destionation__options">
-        <div className="destionation__option">MOON</div>
-        <div className="destionation__option">MARS</div>
-        <div className="destionation__option">EUROPA</div>
-        <div className="destionation__option">TITAN</div>
+        { data.destination.map((item=><div key={item.name} className="destionation__option" onClick={()=>handleDestination(item.name)}>{item.name}</div>))
+        }
       </div>
-      <h3 className="destination__name">MOON</h3>
-      <p className="destionation__text">See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
+      <h3 className="destination__name">{selectedDestination.name}</h3>
+      <p className="destionation__text">{selectedDestination.description}</p>
 
       <div className="destination__distance">
         <div className="distance">AVG. DISTANCE</div>
-        <h5 className="distance__km">384.400 KM</h5>
+        <h5 className="distance__km">{selectedDestination.distance_to_earth}</h5>
       </div>
       <div className="destination__time">
         <div className="time">EST. TRAVEL TIME</div>
-        <h5 className="time__days">3 DAYS</h5>
+        <h5 className="time__days">{selectedDestination.estimated_travel_time}</h5>
       </div>
       </div>
     </>
