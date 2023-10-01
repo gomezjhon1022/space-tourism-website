b16c732535e0afe2e5a78c27cc89ed695cf3d578
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-10-01 16:52:25 +0200

    Add component Technology

---
+++ src/containers/Technology/index.js
@@ -0,0 +1,27 @@
+import { Header } from "../../components/Header";
+import vehicle from '../../assets/technology/image-launch-vehicle-landscape.jpg';
+import './/Technology.scss';
+
+function Technology () {
+  return (
+    <>
+      <Header/>
+      <div className="technology__container">
+      <div className="technology__title"><span>03</span> SPACE LAUNCH 101</div>
+      <img className="technology__image" src={vehicle}></img>
+      <div className="technology__options">
+        <div className="technology__option"><span>1</span></div>
+        <div className="technology__option"><span>2</span></div>
+        <div className="technology__option"><span>3</span></div>
+      </div>
+      <div className="technology__position">THE TERMINOLOGY...</div>
+      <div className="technology__name">LAUNCH VEHICLE</div>
+      <p className="technology__text">A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
+
+      </div>
+
+    </>
+  )
+}
+
+export { Technology };
--- src/routes/App.js
+++ src/routes/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from '../containers/Home/index';
 import { Destination } from '../containers/Destination/index';
 import { Crew } from '../containers/Crew/index';
-
+import { Technology } from '../containers/Technology';
 import './App.scss';
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
             <Route path="/" element={<Home/>}></Route>
             <Route path="/destination" element={<Destination/>}></Route>
             <Route path="/crew" element={<Crew/>}></Route>
+            <Route path="/technology" element={<Technology/>}></Route>
           </Routes>
         </BrowserRouter>
       </div>
