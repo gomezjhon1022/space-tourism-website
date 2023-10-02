ba40d39704d5a6358b3c89e9376aa9935336f9d8
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-10-02 20:41:10 +0200

    Feat: Implemented dynamic display of technology

--- src/containers/Crew/index.js
+++ src/containers/Crew/index.js
@@ -5,8 +5,7 @@ import './Crew.scss';
 function Crew() {
   const [crew, setCrew] = useState("DOUGLAS HURLEY");
   const selectedCrew = data.crew.find(item=>item.name===crew);
-  const crewImage = require(`../../assets/crew/image-${selectedCrew.image.toLowerCase()}.png`);
-
+  const crewImage = require(`../../assets/crew/image-${selectedCrew.image}.png`);
 
   const handleCrew= (name) => {
     setCrew(name);
@@ -14,7 +13,7 @@ function Crew() {
   return (
     <>
       <div className="crew__container">
-      <div className="crew__title"><span>01</span> MEET YOUR CREW</div>
+      <div className="crew__title"><span>02</span> MEET YOUR CREW</div>
       <img className="crew__image" src={crewImage}></img>
       <div className="crew__options">
         {data.crew.map((item=><div key={item.name} className={`crew__option ${item.name===crew? 'selected':''}`} onClick={()=>handleCrew(item.name)} ></div>))
--- src/containers/Technology/index.js
+++ src/containers/Technology/index.js
@@ -1,20 +1,27 @@
-import vehicle from '../../assets/technology/image-launch-vehicle-landscape.jpg';
-import './/Technology.scss';
+import data from '../../assets/data/data.json';
+import { useState } from 'react';
+import './Technology.scss';
 
 function Technology () {
+  const [technology, setTechnology] = useState("LAUNCH VEHICLE");
+  const selectedTechnology = data.technology.find(item=>item.topic===technology);
+  const technologyImage=require(`../../assets/technology/image-${selectedTechnology.image}.jpg`)
+  console.log(selectedTechnology);
+  
+  const handleTechnology= (name)=> {
+    setTechnology(name)
+  }
   return (
     <>
       <div className="technology__container">
       <div className="technology__title"><span>03</span> SPACE LAUNCH 101</div>
-      <img className="technology__image" src={vehicle}></img>
+      <img className="technology__image" src={technologyImage}></img>
       <div className="technology__options">
-        <div className="technology__option"><span>1</span></div>
-        <div className="technology__option"><span>2</span></div>
-        <div className="technology__option"><span>3</span></div>
+        {data.technology.map((item=><div key={item.id} className={`technology__option ${item.topic===technology? 'selected':''}`} onClick={()=>handleTechnology(item.topic)}>{item.id}</div>))}
       </div>
       <div className="technology__position">THE TERMINOLOGY...</div>
-      <div className="technology__name">LAUNCH VEHICLE</div>
-      <p className="technology__text">A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
+      <div className="technology__name">{selectedTechnology.topic}</div>
+      <p className="technology__text">{selectedTechnology.description}</p>
 
       </div>
 
