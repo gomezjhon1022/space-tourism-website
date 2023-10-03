4cdc98245dcc5b32ea4b29f84569bbeddf890624
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-10-03 21:20:08 +0200

    Fix: Styles in view mobile and tablet

--- src/containers/Destination/index.js
+++ src/containers/Destination/index.js
@@ -13,23 +13,23 @@ function Destination() {
   return (
     <>
       <div className="destination__container">
-      <div className="destination__title"><span>01</span> PICK YOUR DESTINATION</div>
-      <img className="destination__image" src={destinationImage}></img>
-      <div className="destionation__options">
-        { data.destination.map((item=><div key={item.name} className={`destionation__option ${item.name === destination ? 'selected':''} `} onClick={()=>handleDestination(item.name)}>{item.name}</div>))
-        }
-      </div>
-      <h3 className="destination__name">{selectedDestination.name}</h3>
-      <p className="destionation__text">{selectedDestination.description}</p>
+        <div className="destination__title"><span>01</span> PICK YOUR DESTINATION</div>
+        <img className="destination__image" src={destinationImage}></img>
+        <div className="destionation__options">
+          { data.destination.map((item=><div key={item.name} className={`destionation__option ${item.name === destination ? 'selected':''} `} onClick={()=>handleDestination(item.name)}>{item.name}</div>))
+          }
+        </div>
+        <h3 className="destination__name">{selectedDestination.name}</h3>
+        <p className="destionation__text">{selectedDestination.description}</p>
 
-      <div className="destination__distance">
-        <div className="distance">AVG. DISTANCE</div>
-        <h5 className="distance__km">{selectedDestination.distance_to_earth}</h5>
-      </div>
-      <div className="destination__time">
-        <div className="time">EST. TRAVEL TIME</div>
-        <h5 className="time__days">{selectedDestination.estimated_travel_time}</h5>
-      </div>
+        <div className="destination__distance">
+          <div className="distance">AVG. DISTANCE</div>
+          <h5 className="distance__km">{selectedDestination.distance_to_earth}</h5>
+        </div>
+        <div className="destination__time">
+          <div className="time">EST. TRAVEL TIME</div>
+          <h5 className="time__days">{selectedDestination.estimated_travel_time}</h5>
+        </div>
       </div>
     </>
   )
